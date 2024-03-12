6e7fe77b441cd77c8d3d2643914670e602579b2a
Arthur Willyams <106842143+r2WillDev@users.noreply.github.com>
2024-03-12 18:02:37 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+string n = "Ribeirao preto";
+
+StringBuilder sb = new StringBuilder();
+
+for(var i = n.Length - 1; i >= 0; i--){
+    sb.Append(n[i]);
+}
+
+Console.WriteLine(sb.ToString());
+
